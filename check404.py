c12c810705b249fbd6c73c8c3d9f2b92c5410133
ashivliving <ashivliving@gmail.com>
2016-07-11 16:26:00 +0530

    404 Done

--- check404.py
+++ check404.py
@@ -1,9 +1,30 @@
 from selenium import webdriver
 import time
 driver = webdriver.PhantomJS()
-driver.get('http://www.mytokri.com')
+baseUrl = "http://www.mytokri.com/"
+driver.get(baseUrl)
 driver.maximize_window()
 #============> Check 404 on everypage <============
+error404 = "MyTokri - 404 Error Page "
+error404 = error404.lower()
 links = driver.find_elements_by_xpath("//*[@href]")
-print len(links)
+urls = []
+for element in links:
+    link =  element.get_attribute("href")
+    url = str(link)
+    if (url.find(baseUrl) != -1):
+        urls.append(url)
+
+print len(urls)
+
+for url in urls:
+    driver.get(url)
+    title = driver.title
+    title = str(title).lower()
+    if (title.find(error404) == -1):
+        #print "Correct URL - " + url
+        continue
+    else:
+        print "Error at URL - " + url
+
 driver.quit()
---
+++ test.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+import time
+# ============> Check 404 on everypage <============
+a = "my name is ashiv gupta"
+b = "ashivg"
+array = []
+array.append("Hey")
+print array
+array.append("You")
+print array[0]
